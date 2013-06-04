5eaa70382331b1798f35bba3162587c005714f6b
Ray H <rymndhng@gmail.com>
2013-06-03 17:21:54 -0700

    initial ruby commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArtStatic::Application.config.secret_token = '879ba5ad7705d82b9ead0081dc0ac9f214fde97196cdfee6ed912ba8875446142c2c60ca41b58dc58eee07b8f05b450aceb11a537aa85b7259cb429a825e1622'
